2b5edd80c8490767b645d5440f9e16c0f8cc848d
Артуров Герман <aptypob@mail.ru>
2021-08-12 22:17:02 +0400

    refactoring token

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .views import UserViewSet, SignupAPIView, TokenAPIView, signup
+from .views import UserViewSet, signup, token
 
 v1 = routers.DefaultRouter()
 v1.register('users', UserViewSet)
@@ -10,6 +10,6 @@ v1.register('users', UserViewSet)
 urlpatterns = [
     path('v1/', include(v1.urls)),
     path('v1/auth/signup/', signup, name='signup'),
-    path('v1/auth/token/', TokenAPIView.as_view(), name='login'),
+    path('v1/auth/token/', token, name='login'),
     path('v1/auth/admin/', TokenObtainPairView.as_view())  # for admin token
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets, filters
-from rest_framework.views import APIView
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
 
 from reviews.models import User
 from .serializers import (
     AdminUserSerializer, TokenSerializer, SignupSerializer, UserSerializer
 )
 from .permissions import IsRoleAdmin
-from .mails import send_confirmation_code
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -34,7 +33,7 @@ class UserViewSet(viewsets.ModelViewSet):
                 request.user, data=request.data, partial=True
             )
             serializer.is_valid(raise_exception=True)
-            serializer.save(instance=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -57,43 +56,38 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class SignupAPIView(APIView):
-    permission_classes = (AllowAny,)
-
-    def post(self, request):
-        serializer = SignupSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            send_confirmation_code(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup(request):
+    serializer = SignupSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        send_confirmation_code(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-def signup(request):
-    if request.method == 'POST':
-        serializer = SignupSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            send_confirmation_code(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+def token(request):
+    serializer = TokenSerializer(data=request.data)
+    if serializer.is_valid():
+        username = serializer.data['username']
+        user = get_object_or_404(User, username=username)
+        confirmation_code = serializer.data['confirmation_code']
+        if default_token_generator.check_token(user, confirmation_code):
+            token = RefreshToken.for_user(user)
+            return Response(
+                {'token': str(token.access_token)},
+                status=status.HTTP_200_OK
+            )
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TokenAPIView(APIView):
-    permission_classes = (AllowAny,)
-
-    def post(self, request):
-        serializer = TokenSerializer(data=request.data)
-        if serializer.is_valid():
-            username = serializer.data['username']
-            user = get_object_or_404(User, username=username)
-            confirmation_code = serializer.data['confirmation_code']
-            if default_token_generator.check_token(user, confirmation_code):
-                token = RefreshToken.for_user(user)
-                return Response(
-                    {'token': str(token.access_token)},
-                    status=status.HTTP_200_OK
-                )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def send_confirmation_code(user):
+    confirmation_code = default_token_generator.make_token(user)
+    subject = 'Код подтверждения YaMDb'
+    message = f'{confirmation_code} - ваш код для авторизации на YaMDb'
+    admin_email = 'Admin@YaMDb.ru',
+    user_email = [user.email]
+    return send_mail(subject, message, admin_email, user_email)
